0601f083b717fed8058a5c94ce6f20825930e081
Christine Lo <christine.lo.hayes@gmail.com>
2022-01-28 22:31:55 -0500

    verifyjwt

--- server.js
+++ server.js
@@ -1,28 +1,32 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const jwt=require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 app.use(express.json())
 const posts = [
   {
-    username:'bob',
-    title:'post1'
+    username: 'bob',
+    title: 'post1'
   },
   {
-    username:'jan',
-    title:'post2'
+    username: 'jan',
+    title: 'post2'
   }
 ]
-app.get('/posts', (req, res) => {
-res.json(posts)
-
+app.get('/posts', authenticateToken, (req, res) => {
+  res.json(posts.filter(post => post.username === req.user.name))
 })
 
-app.post('/login',(req,res)=>{
-  //Authenticate User
-  const username =req.body.username
-  const user ={name:username}
-  const accessToken= jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
-  res.json({accessToken:accessToken})
-})
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers['authorization']
+  const token = authHeader && authHeader.split(' ')[1]
+  if (token == null) return res.sendStatus(401)
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    console.log(err)
+    if (err) return res.sendStatus(403)
+    req.user = user
+    next()
+  })
+}
 app.listen(3000)
